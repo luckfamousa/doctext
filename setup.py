0c53459b80e0ca3a671f686d212b7b4635d89c26
Felix Nensa <felix.nensa@uk-essen.de>
2024-01-28 22:46:38 +0100

    Added tesseract language option

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='doctext',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     license='MIT',
     long_description=open('README.md').read(),
